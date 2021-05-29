8b3631233779e6d675156dd8f4156e1420472ef4
Bassam Ismail <skippednote@skippednote.dev>
2021-05-29 10:46:46 +0530

    AXE-0: Scaffold new Drupal project with theme.

---
+++ web/themes/custom/drupalastic/dist/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+// Example Behavior
+//
+// (($, Drupal) => {
+//   Drupal.behaviors.changeThis = {
+//     attach(settings, context) {
+//       if (context) {
+//         $(context);
+//       }
+//       if (settings) {
+//         (settings.card as { [key: string]: number }).toString();
+//       }
+//     },
+//   };
+// })(jQuery, Drupal);
---
+++ web/themes/custom/drupalastic/gulp-tasks/scss.js
@@ -0,0 +1,19 @@
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
+const sass = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob');
+
+const postCSSOptions = [autoprefixer()];
+// const rtl = require("postcss-rtl");
+// postCSSOptions.push(rtl())
+
+module.exports = (gulp, config) => {
+  gulp.task('scss', () =>
+    gulp
+      .src(...config.scss.source)
+      .pipe(sassGlob())
+      .pipe(sass(config.scss.options).on('error', sass.logError))
+      .pipe(postcss(postCSSOptions))
+      .pipe(gulp.dest(config.scss.destination)),
+  );
+};
