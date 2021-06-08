7900c964d13d9776861b3630f18bdf4c62ba90ca
drupalastic <vivsurat@gmail.com>
2021-06-08 22:22:10 +0530

    AXL-00: Base setup and card example.

--- web/themes/custom/drupalastic/gulp-tasks/images.js
+++ web/themes/custom/drupalastic/gulp-tasks/images.js
@@ -9,6 +9,12 @@ module.exports = (gulp, config) => {
           imagemin.gifsicle({ interlaced: true }),
           imagemin.mozjpeg({ quality: 75, progressive: true }),
           imagemin.optipng({ optimizationLevel: 5 }),
+          imagemin.svgo({
+            plugins: [
+              { removeViewBox: true },
+              { cleanupIDs: false }
+            ]
+          }),
         ]),
       ),
   );
